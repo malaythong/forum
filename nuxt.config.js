b1478b76d481115f6ff866582d0f95c4705ceb45
Binh-2060 <binhxayxana@gmail.com>
2023-07-20 21:42:13 +0700

    git config apollo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,62 +1,81 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - project',
-    title: 'project',
+    titleTemplate: "%s - project",
+    title: "project",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '~/plugins/apollo.js', mode: 'client' },
-  ],
+  // plugins: [{ src: "~/plugins/apollo.js", mode: "client" }],
+
   apollo: {
+    cookieAttributes: {
+      expires: 7,
+    },
+
+    defaultOptions: {
+      $query: {
+        loadingKey: "loading",
+        fetchPolicy: "no-cache",
+        errorPolicy: "all",
+      },
+    },
     clientConfigs: {
-      default: '~/plugins/apollo.js',
+      default: {
+        // YOUR ENDPOINT OF YOUR APOLLO CLIENT OR HASURA ENDPOINT
+        httpEndpoint: "https://big-baboon-88.hasura.app/v1/graphql",
+        wsEndpoint: "wss://big-baboon-88.hasura.app/v1/graphql",
+        tokenName: "access_token",
+        includeNodeModules: true,
+        authenticationType: "Bearer",
+        httpLinkOptions: {
+          headers: {
+            "content-type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,PUT,PATCH,OPTIONS,DELETE,POST",
+          },
+        },
+      },
     },
   },
-  
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: false,
       themes: {
         light: {
-          primary: '#595CBC',
+          primary: "#595CBC",
           secondary: colors.grey.darken1,
           accent: colors.shades.black,
-          error: '#DA5644'
+          error: "#DA5644",
         },
         dark: {
           primary: colors.blue.darken2,
@@ -65,13 +84,12 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
